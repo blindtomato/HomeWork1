d2a58975d084522b623f5be2562b3205d053b3d8
Yudin Pavel <yudin.p.s@gmail.com>
2022-10-30 22:39:35 +0700

    feat: add solution of task 6

---
+++ homework/HW6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number % 2 == 0)
+    Console.WriteLine("Введенное вами число четное");
+else Console.WriteLine("Введенное вами число нечетное");
