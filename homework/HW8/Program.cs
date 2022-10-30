26046d1000dbd0b10c93d6e367080266851100eb
Yudin Pavel <yudin.p.s@gmail.com>
2022-10-30 22:52:15 +0700

    feat: add solution of task 8

---
+++ homework/HW8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int count = 1;
+
+while (count <= number)
+{
+    if (count % 2 == 0)
+        Console.Write(count + " ");
+    count++;
+}
