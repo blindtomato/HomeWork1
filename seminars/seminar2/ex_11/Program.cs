a4a5cd0bae629decba2f08d3f015d0d1e6b645fd
Yudin Pavel <yudin.p.s@gmail.com>
2022-10-31 20:10:19 +0700

    feat: add solution of task 11

---
+++ seminars/seminar2/ex_11/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = new Random().Next(100,1000);
+Console.WriteLine($"Сгенерировано число {n}");
+
+int n1 = n / 100;
+int n2 = (n / 10) % 10;
+int n3 = n % 10;
+
+Console.WriteLine($"{n1}{n3}");
+Console.WriteLine(n2);
