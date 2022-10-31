a96e8a5cf6f7f22c9f9d483fcd8bcbb8279e8e65
Yudin Pavel <yudin.p.s@gmail.com>
2022-10-31 20:32:51 +0700

    feat: add solution of task12

---
+++ seminars/seminar2/ex_12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+   Console.WriteLine("кратно"); 
+}
+else 
+{
+   Console.WriteLine($"не кратно, остаток {number1 % number2}"); 
+}
